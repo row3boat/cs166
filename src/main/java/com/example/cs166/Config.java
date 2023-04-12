bde481ff46d0d3bdc914e9d066423ac83e167692
row3boat <row3boat@gmail.com>
2023-04-12 16:23:35 -0700

    Initial commit

---
+++ src/main/java/com/example/cs166/Config.java
@@ -0,0 +1,15 @@
+package com.example.cs166;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
---
+++ src/main/java/com/example/cs166/Cs166Application.java
@@ -0,0 +1,13 @@
+package com.example.cs166;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs166Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs166Application.class, args);
+    }
+
+}
